7d2091f6eab0451dda6be32dc51bc7d2fadc786d
pvaiciunas <p.l.vaiciunas@gmail.com>
2014-04-25 16:21:39 -0400

    Assignment Solution
    This contains the new code that satisfies the solution to the assignment
    problem

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions help aid in reducing computations for 
+## matrix inversion by caching the solution of an inversion, and 
+## then only recomputing if the solution does not yet exist.
+## It is assumed that all matrices passed to these functions
+## are invertible.
 
-## Write a short comment describing this function
+## This function creates a list object that contains the necessary
+## functions to set and retrieve the solution to a matrix inversion
 
 makeCacheMatrix <- function(x = matrix()) {
+	
+	solution <- NULL
+	setMatrix <- function(y) {
+		x <<- y
+		solution <<- NULL
+	}
+	
+	getMatrix <- function() x
+	setSolution <- function(solver) solution <<- solver
+	getSolution <- function() solution
 
+	list(setMatrix = setMatrix, 
+		getMatrix = getMatrix,
+		setSolution = setSolution,
+		getSolution = getSolution)
+		
 }
 
 
-## Write a short comment describing this function
+## This function solves the matrix passed to it by the function above.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	solution <- x$getSolution()
+	if(!is.null(solution)) {
+		message("getting cached solution")
+		return(solution)
+	}
+	
+	data <- x$getMatrix()
+	solution <- solve(data, ...)
+	x$setSolution(solution)
+	solution
 }
+		
+			
+		
